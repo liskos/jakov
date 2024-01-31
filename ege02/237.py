9bc82e8fdb0cead46020b5ea52b24e824ad9c6c6
kos <liskosnik@yandex.ru>
2024-01-31 19:29:35 +0300

    31 01 2024

--- ege02/237.py
+++ ege02/237.py
@@ -2,5 +2,5 @@ for x in 0,1:
     for y in 0,1:
         for z in 0,1:
             for w in 0,1:
-                f=(z==(not(not w or x)))or y
+                f=not(z==(not w or x))or y
                 print(x,y,z,w,"!",int(f))
