e09cb4296a983ed567967e418b159dbd8b6837fc
user <@>
2024-02-07 15:35:08 +0300

    kk

--- ege02/266.py
+++ ege02/266.py
@@ -3,5 +3,4 @@ for x in 0,1:
         for z in 0,1:
             for w in 0,1:
                 f=((not x or y)or(z==x))and(not w or z)
-                if f:
-                    print(x,y,z,w,"!",int(f))
+                print(x,y,z,w,"!",int(f))
